40dff058c72c75cc6211bae28b5fbcf22166be24
Justin Sabelko <justin.sabelko@cru.org>
2018-09-27 14:03:34 -0500

    Update Rails to 4.2.10; security updates

---
+++ config/initializers/rollbar.rb
@@ -0,0 +1,6 @@
+Rollbar.configure do |config|
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.enabled = Rails.env.production?
+
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
+end
