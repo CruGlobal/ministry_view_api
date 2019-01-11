6bdb0cbce5b89103ba9b1d4e4160324b84f59300
Justin Sabelko <justin.sabelko@cru.org>
2019-01-11 13:53:25 -0600

    Get specs passing

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module MinistryViewApi
       g.javascripts false
     end
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*',
